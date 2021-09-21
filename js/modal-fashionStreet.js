bb936551b546d9c767177fbc7ad6d3840761be23
Waafi Adam <abdullah.waafi@gmail.com>
2021-09-21 17:03:14 +0800

    fix project modal video playing

---
+++ js/modal-fashionStreet.js
@@ -0,0 +1,28 @@
+const modalParent = document.querySelector('.modal-parent')
+const openModalButton = document.querySelector('.open-modal-button');
+
+openModalButton.addEventListener('click', ()=>{
+  modalParent.innerHTML = `
+      <div class="video-section-container  modal">
+        <button class="close-modal-button">&times;</button>
+        <div class="video-section-vid">
+          <iframe class="portfolio-vid" src="https://www.youtube.com/embed/sPje4CE8z7c?rel=0;&loop=1&autoplay=1"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+        </div>
+      </div>
+      <div id="overlay-video"></div>
+  `
+  const closeModalButton = document.querySelector('.close-modal-button');
+  const overlay = document.getElementById('overlay-video');
+
+
+  closeModalButton.addEventListener('click', ()=>{
+    modalParent.innerHTML = ''
+  });
+  overlay.addEventListener('click', ()=>{
+    modalParent.innerHTML = ''
+  });
+});
+
